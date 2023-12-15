09e4475bed9017cdfbc32f50908f4256be94b9f7
lukmandev01 <lukmandev01@gmail.com>
2023-12-16 02:02:58 +0700

    12.16.23

--- mahasiswa.php
+++ mahasiswa.php
@@ -74,7 +74,7 @@
           <div class="card my-4">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
               <div class="bg-gradient-warning shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3">Dashboard</h6>
+                <h6 class="text-white text-capitalize ps-3">Data Dashboard</h6>
               </div>
             </div>
             <div class="card-body px-0 pb-2">
